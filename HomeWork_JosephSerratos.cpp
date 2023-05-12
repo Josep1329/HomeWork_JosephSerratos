c5fedb1efea7149dc0fdf14c1f58de861457cc39
Josep1329 <Joseph.serratos@uniat.edu.mx>
2023-05-11 18:51:05 -0600

    Juego del Ahorcado

--- HomeWork_JosephSerratos.cpp
+++ HomeWork_JosephSerratos.cpp
@@ -1,148 +1,143 @@
-#include <iostream>
-#include<cstdlib>
-
+#include "iostream"
+#include "string.h"
+#include "stdlib.h"
+#include "time.h"
+#include "windows.h"
 using namespace std;
 
-void RockPaperScissors();
-void replay();
-int main()
-{
-	int RPS;
+int main() {
+INICIO:
+    string palabra = " ";
+    string palabraMostrada = " ";
+    int vidas = 0;
+    int nivel = 0;
+    int intentos = 0;
+    string letra = " ";
+    bool fin = false;
+    bool v = false;
 
-	cout << "Welcome to Rock, Paper, Scissors\n Wish to start?" << endl << "1  Rock, Paper & Scissors" << endl;
-	cin >> RPS;
+    cout << endl;
+    cout << "         Bienvenidos el Ahorcado :3" << endl;
+    cout << endl;
+    cout << "   Este juego consiste en poder adivinar" << endl;
+    cout << "   toda la palabra, letra por letra :v." << endl;
+    cout << endl;
+NIVEL:
+    cout << endl;
+    cout << "   selecciona el nivel de juego :>" << endl;
+    cout << endl;
+    cout << "   Nivel             Vidas           Escribe" << endl;
+    cout << endl;
+    cout << "   Facil             8               1" << endl;
+    cout << "   Medio             4               2" << endl;
+    cout << "   Muy Dificil       1               3" << endl;
+    cout << endl;
+    cout << "   Ingresa el tu Nivel: "; cin >> nivel; cout << endl;
 
-	switch (RPS)
-	{
+    switch (nivel) {
+    case 1:
+        palabra = "el ahorcado";
+        palabraMostrada = "__ ________";
+        vidas = 8;
+        fin = false;
+        intentos = 0;
+        system("cls");
+        break;
+    case 2:
+        palabra = "un juego";
+        palabraMostrada = "__ _____";
+        vidas = 4;
+        fin = false;
+        intentos = 0;
+        system("cls");
+        break;
+    case 3:
+        palabra = "hecho";
+        palabraMostrada = "_____";
+        vidas = 1;
+        fin = false;
+        intentos = 0;
+        system("cls");
+        break;
+    default:
+        system("cls");
+        cout << "   escribe un nivel valido entre 1 y 3" << endl;
+        goto NIVEL;
+        break;
+    }
 
-	case 1:
-		RockPaperScissors();
-		break;
-	default:
-		break;
-	}
-}
+    cout << "                 El Ahorcado" << endl;
+    cout << "     Intentos:" << intentos << "      Vidas: " << vidas << endl;
+    cout << endl;
+    cout << "palabra: " << palabraMostrada << endl;
+    cout << endl;
+    ///* logica del juego *///
+    while (fin == false) {
+    LETRA:
+        cout << "ingresa una letra: "; cin >> letra; cout << endl;
+        cout << endl;
+        if (letra.size() == 0) {
+            cout << "ingresa una letra" << endl;
+            goto LETRA;
+        }
+        else
+            if (letra.size() == 1) {
+                for (int i = 0; i < palabra.size(); i++) {
+                    if (letra[0] == palabra[i]) {
+                        palabraMostrada[i] = palabra[i];
+                        v = true;
+                    }
+                }
+                if (v == false) { vidas--; }
+                intentos++;
+                v = false;
+            }
+            else {
+                cout << "ingresa solo una letra" << endl;
+                goto LETRA;
+            }
+        if (palabraMostrada == palabra) {
+            system("cls");
+            cout << "                 El Ahorcado" << endl;
+            cout << "     Intentos:" << intentos << "      Vidas: " << vidas << endl;
+            cout << "         Palabra: " << palabra << endl;
+            cout << endl;
+            cout << "                     HAZ GANADO" << endl;
+            cout << "                     Winner!!!" << endl;
+            cout << endl;
+            Sleep(3000);
+            system("cls");
+            cout << endl;
+            cout << "                 Intentalo nuevamente" << endl;
+            Sleep(1000);
+            system("cls");
+            goto INICIO;
+        }
+        if (vidas == 0) { fin = true; }
+        letra = " ";
+        system("cls");
+        cout << "                 El Ahorcado" << endl;
+        cout << "     Intentos:" << intentos << "      Vidas: " << vidas << endl;
+        cout << endl;
+        cout << "palabra: " << palabraMostrada << endl;
+        cout << endl;
 
+        if (fin == true) {
+            system("cls");
+            cout << "                 El Ahorcado" << endl;
+            cout << "     Intentos:" << intentos << "      Vidas: " << vidas << endl;
+            cout << endl;
+            cout << "                     GAME OVER" << endl;
+            cout << "                     Has perdido" << endl;
+            cout << endl;
+            Sleep(2000);
+            system("cls");
+            cout << "Intentalo nuevamente" << endl;
+            Sleep(1000);
+            system("cls");
+            goto INICIO;
+        }
+    }
 
-
-void RockPaperScissors()
-{
-	int counterPlayer = 0;
-	int counterEnemy = 0;
-	srand(time(NULL));
-	int again = 0;
-	do
-	{
-		do
-		{
-			system("cls");
-			int enemy = (rand() % 3) + 1;
-			int option = 0;
-
-			cout << enemy << endl;
-			if (enemy == 1)
-			{
-				cout << "   The Computer chose rock!" << endl;
-			}
-			else if (enemy == 2)
-			{
-				cout << "   The computer chose paper!" << endl;
-			}
-			else if (enemy == 3)
-			{
-				cout << "   The computer scissors!" << endl;
-			}
-
-			cout << endl;
-
-			cout << "Player: " << counterPlayer << "      " << "CPU: " << counterEnemy << endl;
-			cout << "Choose your option" << endl << "1 - rock" << endl << "2 - paper" << endl << "3 - scissors" << endl;
-			cin >> option;
-
-			switch (option)
-			{
-			case 1:
-				if (option == 1 && enemy == 2)
-				{
-					cout << "You lose" << endl;
-					counterEnemy += 1;
-				}
-				else if (option == enemy)
-				{
-					cout << "You tied" << endl;
-				}
-				else if (option == 1 && enemy == 3)
-				{
-					cout << "You win" << endl;
-					counterPlayer += 1;
-				}
-				break;
-			case 2:
-				if (option == 2 && enemy == 3)
-				{
-					cout << "You lose" << endl;
-					counterEnemy += 1;
-				}
-				else if (option == enemy)
-				{
-					cout << "You tied" << endl;
-				}
-				else if (option == 2 && enemy == 1)
-				{
-					cout << "You win" << endl;
-					counterPlayer += 1;
-				}
-				break;
-			case 3:
-				if (option == 3 && enemy == 1)
-				{
-					cout << "You lose" << endl;
-					counterEnemy += 1;
-				}
-				else if (option == enemy)
-				{
-					cout << "You tied" << endl;
-				}
-				else if (option == 3 && enemy == 2)
-				{
-					cout << "You win" << endl;
-					counterPlayer += 1;
-				}
-				break;
-			default:
-				break;
-			}
-			system("pause");
-		} while (counterEnemy < 2 && counterPlayer < 2);
-		cout << endl << "           Final Score" << endl << "Player: " << counterPlayer << "      " << "CPU: " << counterEnemy << endl;
-		system("pause");
-		system("cls");
-
-		replay();
-	} while (again = !1);
-}
-
-void replay()
-{
-	int again = 0;
-
-	cout << "Do you want to play again?" << endl << "0 - yes" << endl << "1 - no" << endl;
-	cin >> again;
-	if (again == 0)
-	{
-		return RockPaperScissors();
-	}
-	else if (again == 1)
-	{
-		cout << "Thank you for playing!" << endl;
-		return;
-	}
-	else
-	{
-		if (again > 1 || again < 0)
-		{
-			cout << "Invalid option, the program will close" << endl;
-
-		}
-	}
+    return 0;
 }
