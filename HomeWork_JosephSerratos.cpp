f212c673961817c2d9320136c056407aa5c60c07
Josep1329 <Joseph.serratos@uniat.edu.mx>
2023-05-25 11:03:42 -0600

    El gato

--- HomeWork_JosephSerratos.cpp
+++ HomeWork_JosephSerratos.cpp
@@ -1,143 +1,507 @@
-#include "iostream"
-#include "string.h"
-#include "stdlib.h"
-#include "time.h"
-#include "windows.h"
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+
 using namespace std;
 
-int main() {
-INICIO:
-    string palabra = " ";
-    string palabraMostrada = " ";
-    int vidas = 0;
-    int nivel = 0;
-    int intentos = 0;
-    string letra = " ";
-    bool fin = false;
-    bool v = false;
+void loop(char c[3][3]);
 
-    cout << endl;
-    cout << "         Bienvenidos el Ahorcado :3" << endl;
-    cout << endl;
-    cout << "   Este juego consiste en poder adivinar" << endl;
-    cout << "   toda la palabra, letra por letra :v." << endl;
-    cout << endl;
-NIVEL:
-    cout << endl;
-    cout << "   selecciona el nivel de juego :>" << endl;
-    cout << endl;
-    cout << "   Nivel             Vidas           Escribe" << endl;
-    cout << endl;
-    cout << "   Facil             8               1" << endl;
-    cout << "   Medio             4               2" << endl;
-    cout << "   Muy Dificil       1               3" << endl;
-    cout << endl;
-    cout << "   Ingresa el tu Nivel: "; cin >> nivel; cout << endl;
+void Primera_Intro(char c[3][3]);
 
-    switch (nivel) {
-    case 1:
-        palabra = "el ahorcado";
-        palabraMostrada = "__ ________";
-        vidas = 8;
-        fin = false;
-        intentos = 0;
-        system("cls");
-        break;
-    case 2:
-        palabra = "un juego";
-        palabraMostrada = "__ _____";
-        vidas = 4;
-        fin = false;
-        intentos = 0;
-        system("cls");
-        break;
-    case 3:
-        palabra = "hecho";
-        palabraMostrada = "_____";
-        vidas = 1;
-        fin = false;
-        intentos = 0;
-        system("cls");
-        break;
-    default:
-        system("cls");
-        cout << "   escribe un nivel valido entre 1 y 3" << endl;
-        goto NIVEL;
-        break;
-    }
+void Primera_Intro(char c[3][3]);
 
-    cout << "                 El Ahorcado" << endl;
-    cout << "     Intentos:" << intentos << "      Vidas: " << vidas << endl;
-    cout << endl;
-    cout << "palabra: " << palabraMostrada << endl;
-    cout << endl;
-    ///* logica del juego *///
-    while (fin == false) {
-    LETRA:
-        cout << "ingresa una letra: "; cin >> letra; cout << endl;
-        cout << endl;
-        if (letra.size() == 0) {
-            cout << "ingresa una letra" << endl;
-            goto LETRA;
-        }
-        else
-            if (letra.size() == 1) {
-                for (int i = 0; i < palabra.size(); i++) {
-                    if (letra[0] == palabra[i]) {
-                        palabraMostrada[i] = palabra[i];
-                        v = true;
-                    }
-                }
-                if (v == false) { vidas--; }
-                intentos++;
-                v = false;
-            }
-            else {
-                cout << "ingresa solo una letra" << endl;
-                goto LETRA;
-            }
-        if (palabraMostrada == palabra) {
-            system("cls");
-            cout << "                 El Ahorcado" << endl;
-            cout << "     Intentos:" << intentos << "      Vidas: " << vidas << endl;
-            cout << "         Palabra: " << palabra << endl;
-            cout << endl;
-            cout << "                     HAZ GANADO" << endl;
-            cout << "                     Winner!!!" << endl;
-            cout << endl;
-            Sleep(3000);
-            system("cls");
-            cout << endl;
-            cout << "                 Intentalo nuevamente" << endl;
-            Sleep(1000);
-            system("cls");
-            goto INICIO;
-        }
-        if (vidas == 0) { fin = true; }
-        letra = " ";
-        system("cls");
-        cout << "                 El Ahorcado" << endl;
-        cout << "     Intentos:" << intentos << "      Vidas: " << vidas << endl;
-        cout << endl;
-        cout << "palabra: " << palabraMostrada << endl;
-        cout << endl;
+void tablero(char c[3][3]);
 
-        if (fin == true) {
-            system("cls");
-            cout << "                 El Ahorcado" << endl;
-            cout << "     Intentos:" << intentos << "      Vidas: " << vidas << endl;
-            cout << endl;
-            cout << "                     GAME OVER" << endl;
-            cout << "                     Has perdido" << endl;
-            cout << endl;
-            Sleep(2000);
-            system("cls");
-            cout << "Intentalo nuevamente" << endl;
-            Sleep(1000);
-            system("cls");
-            goto INICIO;
-        }
-    }
+void Init_J1(char c[3][3]);
+
+void Init_J2(char c[3][3]);
+
+int ganador(char c[3][3]);
+
+int
+main() {
+
+    char c[3][3];
+
+    loop(c);
 
     return 0;
+
 }
+
+void loop(char c[3][3]) 
+{
+
+
+    int i, j;
+
+    i = 0;
+
+    Primera_Intro(c);
+
+    tablero(c);
+
+    do {
+
+        system("clear");
+
+        tablero(c);
+
+        if (i % 2 == 0) {
+
+            Init_J1(c);
+
+        }
+        else {
+
+            Init_J2(c);
+
+        }
+
+        j = ganador(c);
+
+        i++;
+
+    } while (i < 9);
+
+    if (j == 0) {
+
+        cout << "Ha ganado el jugador 1.";
+
+    }
+    else if (j == 1) {
+
+        cout << "Ha ganado el juagdor 2.";
+
+    }
+    else {
+
+        cout << "Empate.";
+
+    }
+
+}
+
+void Primera_Intro(char c[3][3])
+{
+
+    char a;
+
+    a = '1';
+
+    for (int i = 0; i < 3; i++) {
+
+        for (int j = 0; j < 3; j++) {
+
+            c[i][j] = a++;
+
+        }
+    }
+}
+
+
+void
+Init_J1(char c[3][3]) {
+
+    char a;
+
+    int i, j, k;
+
+    do {
+
+        do {
+
+            cout << "Juega jugador 1: "; 
+
+            scanf_s(" %c", &a); 
+        } while (a < '1' || a > '9');
+
+        k = 0;
+
+        switch (a) { 
+        case '1': {
+
+            i = 0;
+
+            j = 0;
+
+            if (c[i][j] == 'X' || c[i][j] == 'O') { 
+                k = 1;
+
+                cout << "Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        case '2': {
+
+            i = 0;
+
+            j = 1;
+
+            if (c[i][j] == 'X' || c[i][j] == 'O') { 
+                k = 1;
+
+                cout << "Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        case '3': {
+
+            i = 0;
+
+            j = 2;
+
+            if (c[i][j] == 'X' || c[i][j] == 'O') { 
+                k = 1;
+
+                cout << "Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        case '4': {
+
+            i = 1;
+
+            j = 0;
+
+            if (c[i][j] == 'X' || c[i][j] == 'O') { 
+                k = 1;
+
+                cout << "Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        case '5': {
+
+            i = 1;
+
+            j = 1;
+
+            if (c[i][j] == 'X' || c[i][j] == 'O') { 
+                k = 1;
+
+                cout << "\"Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        case '6': {
+
+            i = 1;
+
+            j = 2;
+
+            if (c[i][j] == 'X' || c[i][j] == 'O') { 
+                k = 1;
+
+                cout << "Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        case '7': {
+
+            i = 2;
+
+            j = 0;
+
+            if (c[i][j] == 'X' || c[i][j] == 'O') { 
+                k = 1;
+
+                cout << "Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        case '8': {
+
+            i = 2;
+
+            j = 1;
+
+            if (c[i][j] == 'X' || c[i][j] == 'O') { 
+                k = 1;
+
+                cout << "Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        case '9': {
+
+            i = 2;
+
+            j = 2;
+
+            if (c[i][j] == 'X' || c[i][j] == 'O') { 
+                k = 1;
+
+                cout << "Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        }
+
+    } while (k == 1); 
+
+    c[i][j] = 'X';
+
+}
+
+
+void Init_J2(char c[3][3])
+{
+
+
+    char a;
+
+    int i, j, k;
+
+    do {
+
+        do {
+
+            cout << ("Juega jugador 2: "); 
+            scanf_s(" %c", &a);
+        } while (a < '1' || a > '9');
+
+        k = 0;
+
+        switch (a) { 
+        case '1': {
+
+            i = 0;
+
+            j = 0;
+
+            if (c[i][j] == 'X' || c[i][j] == 'O') { 
+                k = 1;
+
+                cout << "Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        case '2': {
+
+            i = 0;
+
+            j = 1;
+
+            if (c[i][j] == 'X' || c[i][j] == 'O') {
+
+                k = 1;
+
+                cout << "Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        case '3': {
+
+            i = 0;
+
+            j = 2;
+
+            if (c[i][j] == 'X' || c[i][j] == 'O') {
+
+                k = 1;
+
+                cout << "Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        case '4': {
+
+            i = 1;
+            j = 0;
+            if (c[i][j] == 'X' || c[i][j] == 'O') {
+
+                k = 1;
+
+                cout << "Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        case '5': {
+
+            i = 1;
+
+            j = 1;
+
+            if (c[i][j] == 'X' || c[i][j] == 'O') {
+
+                k = 1;
+
+                cout << "Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        case '6': {
+
+            i = 1;
+
+            j = 2;
+
+            if (c[i][j] == 'X' || c[i][j] == 'O') {
+
+                k = 1;
+
+                cout << "Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        case '7': {
+
+            i = 2;
+
+            j = 0;
+
+            if (c[i][j] == 'X' || c[i][j] == 'O') {
+
+                k = 1;
+
+                cout << "Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        case '8': {
+
+            i = 2;
+
+            j = 1;
+
+            if (c[i][j] == 'X' || c[i][j] == 'O') {
+
+                k = 1;
+
+                cout << "Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        case '9': {
+
+            i = 2;
+
+            j = 2;
+
+            if (c[i][j] == 'X' || c[i][j] == 'O') {
+
+                k = 1;
+
+                cout << "Casilla ocupada. Intentalo de nuevo.";
+
+            }
+
+            break;
+
+        }
+
+        }
+
+    } while (k == 1);
+
+    c[i][j] = 'O';
+
+}
+
+int ganador(char c[3][3])
+{
+    return 0;
+}
+
+
+void tablero(char c[3][3])
+{
+
+    for (int i = 0; i < 3; i++) {
+
+        for (int j = 0; j < 3; j++) {
+
+            if (j < 2) {
+
+                printf(" %c ", c[i][j]);
+
+            }
+            else {
+
+                printf(" %c ", c[i][j]);
+
+            }
+
+        }
+
+        if (i < 2) {
+
+            cout << "\n---------\n";
+
+        }
+
+    }
+
+    cout << "\n\n";
+
+}
+
